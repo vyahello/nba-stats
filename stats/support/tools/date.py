cbbbf6f9cc4323f0aa26da75b9ece8976b526f9d
Volodymyr Yahello <vyahello@gmail.com>
2018-10-08 15:02:03 +0300

    Add formatting date support

--- stats/support/tools/date.py
+++ stats/support/tools/date.py
@@ -1,19 +1,30 @@
 from datetime import date, timedelta
 
 
+class FormatTime:
+    """Represent date time formatter."""
+
+    def __init__(self, dt: date, fmt: str) -> None:
+        self._dt = dt
+        self._fmt: str = fmt
+
+    def to_str(self) -> str:
+        return self._dt.strftime(self._fmt)
+
+
 class Date:
     """Represent data time object."""
 
-    def __init__(self) -> None:
-        self._format: str = '%m%d%y'
+    def __init__(self, fmt: str = '%m%d%y', days: int = 1, ) -> None:
+        self._fmt: str = fmt
         self._today: date = date.today()
-        self._day: timedelta = timedelta(1)
+        self._diff: timedelta = timedelta(days)
 
     def today(self) -> str:
-        return self._today.strftime(self._format)
+        return FormatTime(self._today, self._fmt).to_str()
 
     def yesterday(self) -> str:
-        return (self._today-self._day).strftime(self._format)
+        return FormatTime(self._today-self._diff, self._fmt).to_str()
 
     def tomorrow(self) -> str:
-        return (self._today+self._day).strftime(self._format)
+        return FormatTime(self._today+self._diff, self._fmt).to_str()
