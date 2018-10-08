7c91a722ed351e4cb504f6f41a0d3a64fde7dd27
Volodymyr Yahello <vyahello@gmail.com>
2018-10-08 23:00:25 +0300

    Add formatting method

--- stats/support/tools/date.py
+++ stats/support/tools/date.py
@@ -1,30 +1,39 @@
 from datetime import date, timedelta
+from abc import ABC, abstractmethod
 
 
-class FormatTime:
+class Format(ABC):
+    """Represent abstraction for formatting."""
+    
+    @abstractmethod
+    def as_str(self) -> str:
+        pass
+
+
+class FormatTime(Format):
     """Represent date time formatter."""
 
     def __init__(self, dt: date, fmt: str) -> None:
         self._dt = dt
         self._fmt: str = fmt
 
-    def to_str(self) -> str:
+    def as_str(self) -> str:
         return self._dt.strftime(self._fmt)
 
 
 class Date:
-    """Represent data time object."""
+    """Represent date time object."""
 
-    def __init__(self, fmt: str = '%m%d%y', days: int = 1, ) -> None:
+    def __init__(self, fmt: str = '%m%d%y', days: int = 1) -> None:
         self._fmt: str = fmt
         self._today: date = date.today()
         self._diff: timedelta = timedelta(days)
 
     def today(self) -> str:
-        return FormatTime(self._today, self._fmt).to_str()
+        return FormatTime(self._today, self._fmt).as_str()
 
     def yesterday(self) -> str:
-        return FormatTime(self._today-self._diff, self._fmt).to_str()
+        return FormatTime(self._today-self._diff, self._fmt).as_str()
 
     def tomorrow(self) -> str:
-        return FormatTime(self._today+self._diff, self._fmt).to_str()
+        return FormatTime(self._today+self._diff, self._fmt).as_str()
