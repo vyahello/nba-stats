b6fd33cff3c09bf7c52c3e4726b24418e6fdb59b
Volodymyr Yahello <vyahello@gmail.com>
2018-10-10 17:45:33 +0300

    Add nba scoreboard date stamp

--- stats/data/endpoints.py
+++ stats/data/endpoints.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import Dict, Any
+from stats.support.tools.date import Date
 from stats.support.web_api.requests import Request, BotRequest
 from stats.support.web_api.urls import Url
 
@@ -11,8 +12,12 @@ class Endpoint(ABC):
     def as_dict(self) -> Dict[Any, Any]:
         pass
 
+    @abstractmethod
+    def name(self) -> str:
+        pass
 
-class Scoreboard(Endpoint):
+
+class _Scoreboard(Endpoint):
     """NBA scoreboard interface endpoint data set."""
 
     def __init__(self, date: str) -> None:
@@ -21,4 +26,44 @@ class Scoreboard(Endpoint):
     def as_dict(self) -> Dict[Any, Any]:
         return self._request.get().as_dict()
 
+    def name(self) -> str:
+        return 'NBA Scoreboard'
 
+
+class TodayScoreboard(Endpoint):
+    """NBA scoreboard interface endpoint data set for today."""
+
+    def __init__(self, date: Date) -> None:
+        self._request: Endpoint = _Scoreboard(date.today())
+
+    def as_dict(self) -> Dict[Any, Any]:
+        return self._request.as_dict()
+
+    def name(self) -> str:
+        return "NBA today's Scoreboard"
+
+
+class YesterdayScoreboard(Endpoint):
+    """NBA scoreboard interface endpoint data set for yesterday."""
+
+    def __init__(self, date: Date) -> None:
+        self._request: Endpoint = _Scoreboard(date.yesterday())
+
+    def as_dict(self) -> Dict[Any, Any]:
+        return self._request.as_dict()
+
+    def name(self) -> str:
+        return "NBA yesterday's Scoreboard"
+
+
+class TomorrowScoreboard(Endpoint):
+    """NBA scoreboard interface endpoint data set for tomorrow."""
+
+    def __init__(self, date: Date) -> None:
+        self._request: Endpoint = _Scoreboard(date.tomorrow())
+
+    def as_dict(self) -> Dict[Any, Any]:
+        return self._request.as_dict()
+
+    def name(self) -> str:
+        return "NBA tomorrow's Scoreboard"
