ddc21d45c9683a80fb17732f46e8577c9754ce86
Volodymyr Yahello <vyahello@gmail.com>
2018-10-10 21:41:05 +0300

    Specify current time

--- stats/support/tools/date.py
+++ stats/support/tools/date.py
@@ -1,37 +1,21 @@
 from datetime import date, timedelta
-from abc import ABC, abstractmethod
 from stats.support.tools.format import FormatTime
+from stats.types import StampTime
 
 
-class Date(ABC):
-    """Represent abstraction of date interface."""
-
-    @abstractmethod
-    def today(self) -> str:
-        pass
-
-    @abstractmethod
-    def yesterday(self) -> str:
-        pass
-
-    @abstractmethod
-    def tomorrow(self) -> str:
-        pass
-
-
-class DateStamp(Date):
+class Date(StampTime):
     """Represent date time object."""
 
-    def __init__(self, fmt: str = '%Y%m%d', days: int = 1) -> None:
+    def __init__(self, fmt: str = '%Y%m%d') -> None:
         self._fmt: str = fmt
         self._today: date = date.today()
-        self._diff: timedelta = timedelta(days)
+        self._day: timedelta = timedelta(1)
 
     def today(self) -> str:
         return FormatTime(self._today, self._fmt).as_str()
 
     def yesterday(self) -> str:
-        return FormatTime(self._today - self._diff, self._fmt).as_str()
+        return FormatTime(self._today - self._day, self._fmt).as_str()
 
     def tomorrow(self) -> str:
-        return FormatTime(self._today + self._diff, self._fmt).as_str()
+        return FormatTime(self._today + self._day, self._fmt).as_str()
